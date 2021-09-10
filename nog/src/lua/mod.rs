c7c46c11652537af9d262047e1efa20cf8c9c9e9
TimUntersberger <timuntersberger2@gmail.com>
2021-09-10 20:37:50 +0200

    implement lua api using a macro
    @ramirezmike this way of implementing the lua API is really nice and I
    want to improve this a lot more so we won't have to do the same
    cumbersome copy-paste from the first version.

--- nog/src/lua/mod.rs
+++ nog/src/lua/mod.rs
@@ -46,24 +46,52 @@ impl<'lua> FromLua<'lua> for BarLayout<'lua> {
     }
 }
 
+macro_rules! inject_mapper {
+    ($tx:ident, $wm:ident, $lua:ident, lua) => {
+        $lua
+    };
+    ($tx:ident, $wm:ident, $lua:ident, tx) => {
+        $tx
+    };
+    ($tx:ident, $wm:ident, $lua:ident, wm) => {
+        $wm
+    };
+}
+
+macro_rules! namespace {
+    ($rt:ident, {
+        $(const $const_ident:ident = $expr:expr);*;
+        $(fn $fn_ident:ident ($($arg_n:ident : $arg_t:ty),*) {
+            inject $($inject_ident:ident),*;
+            $($body:tt)*
+        });*;
+    }) => {
+        $($rt.namespace
+            .add_constant(stringify!($const_ident), $expr)?;)*
+
+        #[allow(unused_parens)]
+        {
+            $($rt.namespace
+                .add_function(stringify!($fn_ident), |_tx, _wm, _lua, ($($arg_n),*): ($($arg_t),*)| {
+                    $(let $inject_ident = inject_mapper!(_tx, _wm, _lua, $inject_ident);)*
+                    $($body)*
+                })?;)*
+        }
+    };
+}
+
 pub fn init<'a>(tx: Sender<Event>, wm: Arc<RwLock<WindowManager>>) -> LuaResult<LuaRuntime<'a>> {
     let rt = LuaRuntime::new(tx.clone(), wm.clone())?;
 
-    rt.namespace
-        .add_constant("runtime_path", get_runtime_path().to_str().unwrap())?;
+    namespace!(rt, {
+        const runtime_path = get_runtime_path().to_str().unwrap();
+        const config_path = get_config_path().to_str().unwrap();
+        const version = option_env!("NOG_VERSION").unwrap_or("DEV");
+        const config = ConfigProxy::new(tx);
 
-    rt.namespace
-        .add_constant("config_path", get_config_path().to_str().unwrap())?;
+        fn bind(mode: KeybindingMode, key_combination: KeyCombination, cb: mlua::Function) {
+            inject lua, tx;
 
-    rt.namespace
-        .add_constant("version", option_env!("NOG_VERSION").unwrap_or("DEV"))?;
-
-    rt.namespace.add_function(
-        "bind",
-        |tx,
-         _wm,
-         lua,
-         (mode, key_combination, cb): (KeybindingMode, KeyCombination, mlua::Function)| {
             lua.set_named_registry_value(&key_combination.get_id().to_string(), cb)?;
             tx.send(Event::Action(Action::CreateKeybinding {
                 mode,
@@ -71,49 +99,57 @@ pub fn init<'a>(tx: Sender<Event>, wm: Arc<RwLock<WindowManager>>) -> LuaResult<
             }))
             .unwrap();
             Ok(())
-        },
-    )?;
+        };
 
-    rt.namespace
-        .add_function("unbind", |tx, _wm, _lua, key: String| {
+        fn unbind(key: String) {
+            inject tx;
+
             tx.send(Event::Action(Action::RemoveKeybinding { key }))
                 .unwrap();
             Ok(())
-        })?;
+        };
 
-    rt.namespace
-        .add_function("update_window_layout", |tx, _wm, _lua, (): ()| {
+        fn update_window_layout() {
+            inject tx;
+
             tx.send(Event::RenderGraph).unwrap();
-            Ok(())
-        })?;
 
-    rt.namespace
-        .add_function("bar_set_layout", |tx, _wm, lua, layout: BarLayout| {
+            Ok(())
+        };
+
+        fn bar_set_layout(layout: BarLayout) {
+            inject lua, tx;
+
             lua.set_named_registry_value("left", layout.left)?;
             lua.set_named_registry_value("center", layout.center)?;
             lua.set_named_registry_value("right", layout.right)?;
             tx.send(Event::RenderBarLayout).unwrap();
+
             Ok(())
-        })?;
+        };
 
-    rt.namespace.add_function("exit", |tx, _wm, _lua, (): ()| {
-        tx.send(Event::Exit).unwrap();
-        Ok(())
-    })?;
+        fn exit() {
+            inject tx;
 
-    rt.namespace.add_function(
-        "ws_focus",
-        |tx, _wm, _lua, (ws_id, direction): (Option<WorkspaceId>, Direction)| {
+            tx.send(Event::Exit).unwrap();
+
+            Ok(())
+        };
+
+        fn ws_focus(ws_id: Option<WorkspaceId>, direction: Direction) {
+            inject tx;
+
             tx.send(Event::Action(Action::Workspace(WorkspaceAction::Focus(
                 ws_id, direction,
             ))))
             .unwrap();
-            Ok(())
-        },
-    )?;
 
-    rt.namespace
-        .add_function("ws_get_all", |_tx, wm, _lua, (): ()| {
+            Ok(())
+        };
+
+        fn ws_get_all() {
+            inject wm;
+
             Ok(wm
                 .read()
                 .unwrap()
@@ -121,63 +157,72 @@ pub fn init<'a>(tx: Sender<Event>, wm: Arc<RwLock<WindowManager>>) -> LuaResult<
                 .iter()
                 .map(|w| w.id)
                 .collect::<Vec<_>>())
-        })?;
+        };
 
-    rt.namespace.add_function(
-        "ws_swap",
-        |tx, _wm, _lua, (ws_id, direction): (Option<WorkspaceId>, Direction)| {
+        fn ws_swap(ws_id: Option<WorkspaceId>, direction: Direction) {
+            inject tx;
+
             tx.send(Event::Action(Action::Workspace(WorkspaceAction::Swap(
                 ws_id, direction,
             ))))
             .unwrap();
-            Ok(())
-        },
-    )?;
 
-    rt.namespace
-        .add_function("session_save", |tx, _wm, _lua, name: Option<String>| {
+            Ok(())
+        };
+
+        fn session_save() {
+            inject tx;
+
             tx.send(Event::Action(Action::SaveSession)).unwrap();
-            Ok(())
-        })?;
 
-    rt.namespace
-        .add_function("session_load", |tx, _wm, _lua, name: Option<String>| {
+            Ok(())
+        };
+
+        fn session_load() {
+            inject tx;
+
             tx.send(Event::Action(Action::LoadSession)).unwrap();
-            Ok(())
-        })?;
 
-    rt.namespace
-        .add_function("win_close", |tx, _wm, _lua, win_id: Option<WindowId>| {
+            Ok(())
+        };
+
+        fn win_close(win_id: Option<WindowId>) {
+            inject tx;
+
             tx.send(Event::Action(Action::Window(WindowAction::Close(win_id))))
                 .unwrap();
+
             Ok(())
-        })?;
+        };
 
-    rt.namespace.add_function(
-        "win_is_managed",
-        |tx, wm, _lua, win_id: Option<WindowId>| {
+        fn win_is_managed(win_id: Option<WindowId>) {
+            inject wm;
+
             let id = win_id.unwrap_or_else(|| Api::get_foreground_window().get_id());
-            Ok(wm.read().unwrap().is_window_managed(id))
-        },
-    )?;
 
-    rt.namespace
-        .add_function("win_manage", |tx, _wm, _lua, win_id: Option<WindowId>| {
+            Ok(wm.read().unwrap().is_window_managed(id))
+        };
+
+        fn win_is_managed(win_id: Option<WindowId>) {
+            inject tx;
+
             tx.send(Event::Action(Action::Window(WindowAction::Manage(win_id))))
                 .unwrap();
-            Ok(())
-        })?;
 
-    rt.namespace
-        .add_function("win_unmanage", |tx, _wm, _lua, win_id: Option<WindowId>| {
+            Ok(())
+        };
+
+        fn win_unmanage(win_id: Option<WindowId>) {
+            inject tx;
+
             tx.send(Event::Action(Action::Window(WindowAction::Unmanage(
                 win_id,
             ))))
             .unwrap();
-            Ok(())
-        })?;
 
-    rt.namespace.add_constant("config", ConfigProxy::new(tx))?;
+            Ok(())
+        };
+    });
 
     rt.namespace.register(None)?;
 
