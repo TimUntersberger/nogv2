5177e95cf1a3bdf9f3b39b739be72af59a5ae9d3
TimUntersberger <timuntersberger2@gmail.com>
2021-09-29 18:27:15 +0200

    update register_startup_task

--- tools/register_startup_task.ps1
+++ tools/register_startup_task.ps1
@@ -1,6 +1,6 @@
 Register-ScheduledTask `
   -TaskName "StartNog" `
-  -Trigger (New-ScheduledTaskTrigger -AtStartup) `
+  -Trigger (New-ScheduledTaskTrigger -AtLogon) `
   -User $env:username `
   -Action (New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "start-process -windowstyle hidden nog") `
   -RunLevel Highest `
