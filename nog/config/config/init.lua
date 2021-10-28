54ac9b157ac7f5bcd43f4de895e325748962e40c
TimUntersberger <timuntersberger2@gmail.com>
2021-10-28 13:40:47 +0200

    change: restructure config

--- nog/config/config/init.lua
+++ nog/config/config/init.lua
@@ -1,152 +1,23 @@
-nog.nbind("alt+t", function()
-  if nog.win_is_managed(nil) then
-    nog.win_unmanage(nil)
-  else
-    nog.win_manage(nil)
-  end
-end)
-
-nog.nbind("alt+ctrl+f", function()
-  nog.ws_set_fullscreen(1, not nog.ws_is_fullscreen(1))
-end)
-
-nog.nbind("alt+m", function()
-  nog.win_minimize(nil)
-end)
-
-nog.gbind("alt+escape", function()
-  if nog.is_awake() then
-    nog.hibernate()
-  else
-    nog.awake()
-  end
-end)
-
-nog.nbind("alt+ctrl+r", function()
-  dofile(nog.config_path .. "\\lua\\config.lua")
-end)
-
-nog.nbind("alt+space", function()
-  nog.open_menu()
-end)
-
-nog.nbind("alt+F1", function()
-  nog.update_window_layout()
-end)
-
-nog.nbind_tbl("alt", nog.change_ws, nog.range(9))
-
-nog.nbind_tbl("alt+ctrl", function(ws_id) 
-  nog.move_win_to_ws(nil, ws_id)
-end, nog.range(9))
-
-nog.nbind("alt+0", function()
-  nog.change_ws(10)
-end)
-
-nog.nbind("alt+x", function()
-  nog.exit()
-end)
-
-nog.nbind("alt+h", function()
-  nog.ws_focus(nil, "left")
-end)
-
-nog.nbind("alt+j", function()
-  nog.ws_focus(nil, "down")
-end)
-
-nog.nbind("alt+l", function()
-  nog.ws_focus(nil, "right")
-end)
-
-nog.nbind("alt+k", function()
-  nog.ws_focus(nil, "up")
-end)
-
-nog.nbind("alt+h", function()
-  nog.ws_focus(nil, "left")
-end)
-
-nog.nbind("alt+j", function()
-  nog.ws_focus(nil, "down")
-end)
-
-nog.nbind("alt+l", function()
-  nog.ws_focus(nil, "right")
-end)
-
-nog.nbind("alt+k", function()
-  nog.ws_focus(nil, "up")
-end)
-
-nog.nbind("ctrl+alt+h", function()
-  nog.ws_swap(nil, "left")
-end)
-
-nog.nbind("ctrl+alt+j", function()
-  nog.ws_swap(nil, "down")
-end)
-
-nog.nbind("ctrl+alt+l", function()
-  nog.ws_swap(nil, "right")
-end)
-
-nog.nbind("ctrl+alt+k", function()
-  nog.ws_swap(nil, "up")
-end)
-
-nog.nbind("alt+q", function()
-  nog.win_close(nil)
-end)
-
-local auto_manage = true
-
-nog.nbind("alt+ctrl+m", function()
-  auto_manage = not auto_manage
-end)
-
-local window_handlers = {
-  {
-    when = {
-      title = "^.* - Mozilla Firefox$",
-    },
-    action = {
-      workspace_id = 2
-    }
-  }
-}
-
-nog.on("manage", function(ev)
-  if not ev.manual then
-    if not auto_manage then
-      return
-    end
-
-    local size = nog.win_get_size(ev.win_id)
-
-    if size.width <= 100 or size.height <= 100 then
-      return
+function keybindings(tbl)
+  for mode, bindings in pairs(tbl) do
+    for key, cb in pairs(bindings) do
+      nog.bind(mode, key, cb)
     end
   end
+end
 
-  local title = nog.win_get_title(ev.win_id)
-
-  for _, wh in ipairs(window_handlers) do
-    local matches = true
-
-    for key, val in pairs(wh.when) do
-      if key == "title" and not title:find(val) then
-        matches = false
-        break
+function event_handlers(tbl)
+  for event_name, event_handlers in pairs(tbl) do
+    nog.on(event_name, function(ev)
+      for _, handler in ipairs(event_handlers) do
+        if handler.when(ev) then
+          handler.action(ev)
+          break
+        end
       end
-    end
-
-    if matches then
-      return wh.action
-    end
+    end)
   end
-end)
+end
 
 nog.config.color = 0x2e3440
 nog.config.remove_task_bar = false
@@ -168,5 +39,153 @@ nog.bar_set_layout {
   right = {
     nog.components.datetime("%d.%m.%Y"),
     nog.components.padding(1),
+  }
+}
+
+keybindings {
+  g = {
+    ["alt+escape"] = function()
+      if nog.is_awake() then
+        nog.hibernate()
+      else
+        nog.awake()
+      end
+    end
+  },
+  n = {
+    ["alt+m"] = function()
+      nog.win_minimize(nil)
+    end,
+    ["alt+t"] = function()
+      if nog.win_is_managed(nil) then
+        nog.win_unmanage(nil)
+      else
+        nog.win_manage(nil)
+      end
+    end,
+    ["alt+ctrl+f"] = function()
+      nog.ws_set_fullscreen(1, not nog.ws_is_fullscreen(1))
+    end,
+    ["alt+ctrl+r"] = function()
+      dofile(nog.config_path .. "\\lua\\config.lua")
+    end,
+    ["alt+space"] = function()
+      nog.open_menu()
+    end,
+    ["alt+x"] = function()
+      nog.exit()
+    end,
+    ["alt+q"] = function()
+      nog.win_close(nil)
+    end,
+    ["alt+h"] = function()
+      nog.ws_focus(nil, "left")
+    end,
+    ["alt+j"] = function()
+      nog.ws_focus(nil, "down")
+    end,
+    ["alt+l"] = function()
+      nog.ws_focus(nil, "right")
+    end,
+    ["alt+k"] = function()
+      nog.ws_focus(nil, "up")
+    end,
+    ["ctrl+alt+h"] = function()
+      nog.ws_swap(nil, "left")
+    end,
+    ["ctrl+alt+j"] = function()
+      nog.ws_swap(nil, "down")
+    end,
+    ["ctrl+alt+l"] = function()
+      nog.ws_swap(nil, "right")
+    end,
+    ["ctrl+alt+k"] = function()
+      nog.ws_swap(nil, "up")
+    end,
+    ["alt+1"] = function()
+      nog.change_ws(1)
+    end,
+    ["alt+2"] = function()
+      nog.change_ws(2)
+    end,
+    ["alt+3"] = function()
+      nog.change_ws(3)
+    end,
+    ["alt+4"] = function()
+      nog.change_ws(4)
+    end,
+    ["alt+5"] = function()
+      nog.change_ws(5)
+    end,
+    ["alt+6"] = function()
+      nog.change_ws(6)
+    end,
+    ["alt+7"] = function()
+      nog.change_ws(7)
+    end,
+    ["alt+8"] = function()
+      nog.change_ws(8)
+    end,
+    ["alt+9"] = function()
+      nog.change_ws(9)
+    end,
+    ["alt+0"] = function()
+      nog.change_ws(10)
+    end,
+    ["alt+ctrl+1"] = function()
+      nog.move_win_to_ws(nil, 1)
+    end,
+    ["alt+ctrl+2"] = function()
+      nog.move_win_to_ws(nil, 2)
+    end,
+    ["alt+ctrl+3"] = function()
+      nog.move_win_to_ws(nil, 3)
+    end,
+    ["alt+ctrl+4"] = function()
+      nog.move_win_to_ws(nil, 4)
+    end,
+    ["alt+ctrl+5"] = function()
+      nog.move_win_to_ws(nil, 5)
+    end,
+    ["alt+ctrl+6"] = function()
+      nog.move_win_to_ws(nil, 6)
+    end,
+    ["alt+ctrl+7"] = function()
+      nog.move_win_to_ws(nil, 7)
+    end,
+    ["alt+ctrl+8"] = function()
+      nog.move_win_to_ws(nil, 8)
+    end,
+    ["alt+ctrl+9"] = function()
+      nog.move_win_to_ws(nil, 9)
+    end,
+    ["alt+ctrl+0"] = function()
+      nog.move_win_to_ws(nil, 10)
+    end
+  }
+}
+
+event_handlers {
+  manage = {
+    {
+      when = function(ev)
+        local title = nog.win_get_title(ev.win_id)
+        return title:find("^.* - Notepad$")
+      end,
+      action = function(ev)
+        if ev.ws_id ~= 2 then
+          nog.move_win_to_ws(ev.win_id, 2)
+        end
+      end
+    },
+    {
+      when = function(ev)
+        local size = nog.win_get_size(ev.win_id)
+        return size.width <= 100 or size.height <= 100
+      end,
+      action = function(ev)
+        nog.win_unmanage(ev.win_id)
+      end
+    }
   }
 }
