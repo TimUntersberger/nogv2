57c87aa9e7d076400dfb1d3ecfa7a5f6a577f030
TimUntersberger <timuntersberger2@gmail.com>
2021-10-30 21:18:36 +0200

    feat(#31): log to file in production
    closes #31

--- nog/src/logging.rs
+++ nog/src/logging.rs
@@ -1,11 +1,37 @@
 use std::sync::mpsc::channel;
 use std::thread;
 
+#[cfg(not(debug_assertions))]
+use chrono::Utc;
+#[cfg(not(debug_assertions))]
+use std::fs::OpenOptions;
+#[cfg(not(debug_assertions))]
+use std::io::prelude::*;
+#[cfg(not(debug_assertions))]
+use crate::paths::get_config_path;
+
 pub fn init() -> Result<(), log::SetLoggerError> {
     let (tx, rx) = channel();
+
     thread::spawn(move || {
+        #[cfg(not(debug_assertions))]
+        let mut file = OpenOptions::new()
+            .append(true)
+            .open({
+                let mut path = get_config_path();
+
+                path.push("logs");
+                path.set_file_name(Utc::now().format("%Y_%m_%d").to_string());
+                path.set_extension(".txt");
+
+                path
+            })
+            .unwrap();
+
         while let Ok(msg) = rx.recv() {
-            print!("{}", msg);
+            print!("{}", &msg);
+            #[cfg(not(debug_assertions))]
+            write!(file, "{}", &msg).unwrap();
         }
     });
 
